eebe032db527d91125b0a55642db1c237111bcb9
heigen007 <67007974+Heigen007@users.noreply.github.com>
2024-02-19 20:18:12 +0600

    lab 4

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,120 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  rating: number;
+  url: string;
+  imgURL: string;
+  link: string;
+};
+
+export const products: Product[] = [
+  {
+    id: 3,
+    name: "Мышь LS Pro черный",
+    price: 20500,
+    description: "Мышь LS Pro черный",
+    rating: 5,
+    url: "https://kaspi.kz/shop/p/ls-pro-chernyi-107622516/?c=750000000",
+    imgURL: "https://resources.cdn-kaspi.kz/img/m/p/h2f/h61/82393746636830.png?format=gallery-medium",
+    link: "https://t.me/heigen007"
+  },
+  {
+    id: 4,
+    name: "Kingston SDCS2/64Gb 64 Гб",
+    price: 1850,
+    description: "Карта памяти Kingston SDCS2/64Gb 64 Гб",
+    rating: 5,
+    url: "https://kaspi.kz/shop/p/kingston-sdcs2-64gb-64-gb-100081475/?c=750000000",
+    imgURL: "https://resources.cdn-kaspi.kz/img/m/p/h21/h99/63868015804446.jpg?format=gallery-medium",
+    link: "https://t.me/heigen0070"
+  },
+  {
+    id: 5,
+    name: "Apple iPhone 11",
+    price: 246911,
+    description: "Функциональный и стильный смартфон Apple iPhone 11 в металлическом корпусе способен обеспечить не только повседневное общение и развлечения, но и продуктивную работу.",
+    rating: 5,
+    url: "https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000#!/item",
+    imgURL: "https://cdn.evrika.com/storage/products/images/default/uxr6gPUg0zJgiSLjJkcplapiy1bzlD7tFYs09s3n.jpg.webp",
+    link: "https://t.me/heigen007"
+  },
+  {
+    id: 6,
+    name: "Samsung Galaxy Watch 4",
+    price: 59990,
+    description: "Умные часы Samsung - это не просто гаджет, а ваш верный спутник на пути к достижению задач и самосовершенствованию.",
+    rating: 5,
+    url: "https://kaspi.kz/shop/p/samsung-galaxy-watch-4-classic-sm-r890nzkacis-46-mm-chernyi-chernyi-102170938/?c=750000000#!/item",
+    imgURL: "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h31/h9b/62047597559838/samsung-galaxy-watch-4-classic-sm-r890-cernyj-102170938-1-Container.jpg",
+    link: "https://t.me/heigen007"
+  },
+  {
+    id: 7,
+    name: "Зарядное устройство Apple 20W USB-C",
+    price: 3292,
+    description: "Зарядное устройство Apple 20W USB-C Power Adapter белый",
+    rating: 5,
+    url: "https://kaspi.kz/shop/p/apple-20w-usb-c-power-adapter-belyi-100984093/?c=750000000",
+    imgURL: "https://resources.cdn-kaspi.kz/img/m/p/h95/h0e/63961934823454.jpg?format=gallery-medium",
+    link: "https://t.me/heigen007"
+  },
+  {
+    id: 2,
+    name: "Автомобильная визитка AVTOCHEHLY.KZ 2107221439",
+    price: 205,
+    description: "Cпособ крепления: двухсторонний скотч",
+    rating: 5,
+    url: "https://kaspi.kz/shop/p/apple-watch-series-8-45-mm-aluminum-chernyi-106362847/?c=750000000#!/item",
+    imgURL: "https://resources.cdn-kaspi.kz/img/m/p/ha2/h5a/64527256748062.jpg?format=gallery-medium",
+    link: "https://t.me/heigen007"
+  },
+  {
+    id: 8,
+    name: "Apple AirPods Max",
+    price: 273990,
+    description: "Bluetooth гарнитура Apple AirPods Max сможет полностью изменить ваше представление о полноразмерных наушниках. Данная модель разработана с применением первоклассных материалов и передовых технологий.",
+    rating: 5,
+    url: "https://kaspi.kz/shop/p/apple-airpods-max-sinii-101180956/?c=750000000#!/item",
+    imgURL: "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h84/h0d/46659800498206/apple-airpods-max-sinij-101180956-1-Container.jpg",
+    link: "https://t.me/heigen007"
+  },
+  {
+    id: 9,
+    name: "Яндекс Станция Лайт",
+    price: 26951,
+    description: "Яндекс.Станция Лайт — это яркая и компактная умная колонка, которая управляется голосом и сенсорными кнопками. Она здорово украсит интерьер и сможет развлечь ребенка: матовый корпус приятно держать в руках, а расцветка порадует глаз.",
+    rating: 5,
+    url: "https://kaspi.kz/shop/p/jandeks-stantsija-lait-birjuzovyi-101808994/?c=750000000#!/item",
+    imgURL: "https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h36/h98/49342279876638/yandeks-stantsiya-lait-biryuzovyi-101808994-1.jpg",
+    link: "https://t.me/heigen007"
+  },
+    {
+        id: 10,
+        name: "Apple Watch Series 8",
+        price: 194000,
+        description: "Apple Watch Series 8 45 mm Aluminum черный",
+        rating: 5,
+        url: "https://kaspi.kz/shop/p/apple-watch-series-8-45-mm-aluminum-chernyi-106362847/?c=750000000#!/item",
+        imgURL: "https://resources.cdn-kaspi.kz/img/m/p/h5a/h17/64401042472990.jpg?format=gallery-medium",
+        link: "https://t.me/heigen007"
+    },
+    {
+        id: 11,
+        name: "Apple iPhone 13",
+        price: 291311,
+        description: "Функциональный и стильный смартфон Apple iPhone 13 в металлическом корпусе способен обеспечить не только повседневное общение и развлечения, но и продуктивную работу.",
+        rating: 5,
+        url: "https://kaspi.kz/shop/p/apple-iphone-13-128gb-midnight-chernyi-102298404/?c=750000000",
+        imgURL: "https://resources.cdn-kaspi.kz/img/m/p/h32/h70/84378448199710.jpg?format=gallery-medium",
+        link: "https://t.me/heigen007"
+    },
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
