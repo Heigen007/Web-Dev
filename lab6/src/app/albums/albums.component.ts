4a35296cac1fc44d114f877c8b78ac4b190f781d
heigen007 <67007974+Heigen007@users.noreply.github.com>
2024-03-04 19:18:09 +0500

    Lab 6

---
+++ lab6/src/app/albums/albums.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+
+import { faTrash, faAdd } from '@fortawesome/free-solid-svg-icons';
+
+import { AlbumsService } from './albums.service';
+import { IAlbum } from 'src/models/albums';
+
+import { map} from 'rxjs';
+
+@Component({
+  selector: 'app-albums',
+  templateUrl: './albums.component.html',
+  styleUrls: ['./albums.component.scss']
+})
+export class AlbumsComponent implements OnInit {
+
+  albums: IAlbum[];
+  newAlbumTitle: string;
+  faTrashIcon = faTrash;
+  faAddIcon = faAdd;
+
+  constructor(private albumsService: AlbumsService) {
+    this.albums = [];
+    this.newAlbumTitle = "";
+  }
+
+  ngOnInit(): void {
+    this.albumsService.getAlbums().pipe(
+      map( x=> x.slice(10,20))
+    ).subscribe((response) => {
+        this.albums = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  handleDeleteAlbum(album: IAlbum): void {
+    this.albumsService.deleteAlbum(album.id).subscribe(
+      (response) => {
+        const index = this.albums.indexOf(album);
+        if (index !== -1) {
+          this.albums.splice(index, 1);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  handleAddNewAlbum(): void {
+    if (this.newAlbumTitle === "") {
+      return;
+    }
+    
+    this.albumsService.addAlbum({ userId: this.albums.length + 1, id: this.albums.length + 1, title: this.newAlbumTitle }).subscribe(
+      (response) => {
+        this.albums.push(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
